d88aaa82abdec9439ab9267db261794accac8cb8
KarolinaMaczka <kari.maczka@gmail.com>
2025-03-27 21:01:37 +0100

    gen dataset

--- generating_dataset.py
+++ generating_dataset.py
@@ -1,17 +1,14 @@
 import numpy as np
 def generate_dataset(p, n, d, g, seed=42):
     np.random.seed(seed)
-    indices = np.arange(d)
-    S = g ** np.abs(np.subtract.outer(indices, indices))
+    indices = np.linspace(0, d - 1, d, dtype=int)
+    S = g**np.abs(np.expand_dims(indices, 0) - np.expand_dims(indices, 1))
     mu0 = np.zeros(d)
     mu1 = np.array([1 / (i + 1) for i in range(d)])
-    mu1[0] = 1 
+    mu1[0] = 1
+
     y = np.random.binomial(1, p, size=n)
-    X = np.empty((n, d))
-    for i in range(n):
-        if y[i] == 0:
-            X[i, : ]=np.random.multivariate_normal(mu0, S)
-        else:
-            X[i, :]=np.random.multivariate_normal(mu1, S)
-    
+    X = np.array([np.random.multivariate_normal(mu0, S) if label == 0 
+                else np.random.multivariate_normal(mu1, S) for label in y])
+
     return X, y
