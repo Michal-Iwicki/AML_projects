a5eb9bd58ad467ce0b97627ab4b6dee09d7542d6
KarolinaMaczka <kari.maczka@gmail.com>
2025-03-30 11:14:42 +0200

    docstrings

--- generating_dataset.py
+++ generating_dataset.py
@@ -1,5 +1,16 @@
 import numpy as np
 def generate_dataset(p, n, d, g, seed=42):
+    """
+    generates a binary labeled dataset where features follow d-dimensional multivariate 
+    normal distribution with class-specific means and a shared covariance matrix
+
+    :param p: probability of a sample belonging to class 1
+    :param n: number of samples to generate
+    :param d: dimensions of a feature space
+    :param g: used to construct covariance matrix, determines how quickly the correlation between features decreases as their distance increases
+    :param seed: 
+    :returns: X, y, where X is an array of features and y is an array of binary classes
+    """
     np.random.seed(seed)
     indices = np.linspace(0, d - 1, d, dtype=int)
     S = g**np.abs(np.expand_dims(indices, 0) - np.expand_dims(indices, 1))
@@ -8,7 +19,5 @@ def generate_dataset(p, n, d, g, seed=42):
     mu1[0] = 1
 
     y = np.random.binomial(1, p, size=n)
-    X = np.array([np.random.multivariate_normal(mu0, S) if label == 0 
-                else np.random.multivariate_normal(mu1, S) for label in y])
-
+    X = np.array([np.random.multivariate_normal(mu0, S) if label == 0 else np.random.multivariate_normal(mu1, S) for label in y])
     return X, y
--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,13 @@ import pandas as pd
 from sklearn.calibration import LabelEncoder
 
 def add_permuted_columns(df):
+    """
+    adds permuted versions of existing feature columns to the dataframe 
+    to ensure that the number of features is at least half the number of rows
+
+    :param df: input dataframe with a column named "target" containing a label and feature columns
+    :returns: new dataframe with new permuted columns
+    """
     current_features = df.shape[1] - 1 
     needed_features = int(np.ceil(0.5 * df.shape[0]))
     n_to_add = needed_features - current_features
@@ -29,6 +36,14 @@ def add_permuted_columns(df):
         return df
 
 def assign_target_column(df, target_column, positive_class):
+    """
+    creates a binary column named "target" based on the specified values representing the positive class
+
+    :param df: dataframe containing the original target_column
+    :param target_column: name of the column to be converted into binary labels
+    :param positive_class: list of values that are going to be aggregated to the class labeleb "1"
+    :returns: dataframe with a new 'target' column and without the original target column
+    """
     print(f'Unique values of label column {pd.unique(df[target_column])}')
     df['target'] = (df[target_column].isin(positive_class)).astype(int)
     df.drop(columns=[target_column], inplace=True)
@@ -37,6 +52,7 @@ def assign_target_column(df, target_column, positive_class):
 def fill_na(df):
     """
     fills null values with median
+    :param df: input dataframe 
     """
     na_counts = df.isnull().sum()
     na_columns = na_counts[na_counts > 0]
@@ -46,6 +62,12 @@ def fill_na(df):
     
     
 def delete_corr_columns(df):
+    """
+    removes highly correlated feature columns 
+
+    :param df: input dataframe with feature columns and a column named "target"
+    :returns: dataframe with correlated columns removed
+    """
     corr_matrix = df.drop(columns=['target']).corr().abs()
     upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
     to_drop = [column for column in upper.columns if any(upper[column] > 0.9)]
@@ -55,23 +77,31 @@ def delete_corr_columns(df):
     
     
 def check_feature_sample_ratio(df):
+    """
+    checks if the number of features is at least half of the number of samples and prints the result
+    :param df: input dataframe with feature columns
+    """
     print(f'The number of features is enough: {len(df.columns) * 2 > len(df)}, number of features: {len(df.columns)}')
     
 def split(df):
+    """
+    splits the dataframe into train, validation and test sets with a 35/35/30 ratio
+
+    :param df: dataframe with column named "target"
+    :returns: X, y, X_train, X_valid, X_test, y_train, y_valid, y_test 
+    """
     X = df.drop(columns=['target'])
     y = df['target']
     X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     X_train, X_valid, y_train, y_valid = train_test_split(X_train_val, y_train_val, test_size=0.5, random_state=42)
     return X, y.astype(int), X_train, X_valid, X_test, y_train.astype(int), y_valid.astype(int), y_test.astype(int)
-
-def one_hot_encode_categorical(df):
-    feature_cols = df.columns.drop("target")
-    for col in feature_cols:
-        if not pd.api.types.is_numeric_dtype(df[col]):
-            le = LabelEncoder()
-            df[col] = le.fit_transform(df[col].astype(str))
             
 def drop_categorical(df):
+    """
+    removes all categorical columns from the dataframe except for the 'target' column
+    :param df: input dataframe
+    :returns: dataframe with only numeric columns and a column named "target"
+    """
     target = "target"
     columns_to_keep = [col for col in df.columns if pd.api.types.is_numeric_dtype(df[col]) or col == target]
     print(f"Number of columns to keep {len(columns_to_keep)}, to delete {len(df.columns) - len(columns_to_keep)}")
